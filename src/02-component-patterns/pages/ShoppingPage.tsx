d685b6d2d478fa7ffa52c193c9269f63c7af9cf3
shakharho <berdaddario@gmail.com>
2023-02-16 17:56:11 -0300

    control props pattern implemented

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,63 +1,19 @@
-import { useState } from "react";
 import {
   ProductTitle,
   ProductImage,
   ProductButtons,
   ProductCard,
 } from "../components";
-import { Product } from '../interfaces/index';
+import { products } from "../data";
+import { useShoppingCart } from "../hooks/useShoppingCart";
+
 import "../styles/custom-styles.css";
 
-const products: Product[] = [
-  {
-    id: "1",
-    title: "Coffee Mug",
-    img: "./coffee-mug.png",
-  },
-  {
-    id: "2",
-    title: "Coffee Mug Meme",
-    img: "./coffee-mug2.png",
-  }
-]; 
-
-interface ProductInCart extends Product {
-    count: number
-}
 
 export const ShoppingPage = () => {
-
-  const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({
-    // '1':  { ...products[0], count: 9 },
-  });
-
-  const onProductCountChange = ({ count, product }: { count:number, product: Product } ) => {
-
-    setShoppingCart(( prevShoppingCart ) => {
-
-    const productInCart: ProductInCart = prevShoppingCart[product.id] || { ...product, count: 0 };  
-      if(Math.max( productInCart.count + count, 0) > 0) {
-        productInCart.count += count;  
-        return {
-          ...prevShoppingCart,
-          [product.id]: productInCart
-        }
-      }
-      //si count es 0 ó menor a 0 borra el producto
-      const { [product.id]: itemToDelete, ...rest } = prevShoppingCart;  
-
-      return {...rest};
-    });
-  }
-    //   if(count === 0) {
-
-        // console.log({itemToDelete})
-
-
-    //   return {
-    //     ...prevShoppingCart,
-    //     [product.id] : { ...product, count }
-    // }
+  
+  const { shoppingCart, onProductCountChange } = useShoppingCart();
+  
   return (
     <div>
       <h1 style={{ marginBottom: 25 }}> Shopping Store </h1>
